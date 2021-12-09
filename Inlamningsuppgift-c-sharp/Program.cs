24d6d0823836e55b962c2c00c33f921ece783a3e
Johan Berglund <johan.berglund00@hotmail.com>
2021-12-09 14:05:23 +0100

    Inlamningsuppgift-1

--- Inlamningsuppgift-c-sharp/Program.cs
+++ Inlamningsuppgift-c-sharp/Program.cs
@@ -10,6 +10,36 @@ namespace Inlamningsuppgift_c_sharp
     {
         static void Main(string[] args)
         {
+            bool isOn = true;
+            while (isOn == true)
+
+            Console.WriteLine("Välkommen!");
+            Console.WriteLine("\n");
+
+            int menyVal = 1;
+            switch (menyVal)
+            {
+                case 1:
+                    Console.WriteLine("Lägg till en ny gäst i listan");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Visa inlagda gäster");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Ta bort gäst från listan");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Generera rabattkod");
+                    break;
+
+                case 5:
+                    Console.WriteLine("Avsluta programmet");
+                    isOn = false;
+                    break;
+            }
         }
     }
 }
